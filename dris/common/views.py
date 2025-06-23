f622d950467f09b94bed86d01412a3b1774b2b98
ザティル・イマニ <zatilimani.work@gmail.com>
2025-06-24 03:37:23 +0800

    update home page

--- dris/common/views.py
+++ dris/common/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from ShelterDirectory.models import Shelter
+from DisasterReporting.models import DisasterReport
+from AidRequestManagement.models import AidRequest
+from VolunteerCoordination.models import Volunteer
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'base.html')
+    context = {
+        'shelter_count': Shelter.objects.count(),
+        'disaster_report_count': DisasterReport.objects.count(),
+        'aid_request_count': AidRequest.objects.count(),
+        'volunteer_count': Volunteer.objects.count(),
+    }
+    return render(request, 'components/home/index.html', context)

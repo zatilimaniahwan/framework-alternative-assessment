5c676b3084124d19e3d23cebce51ea8d7026324d
ザティル・イマニ <zatilimani.work@gmail.com>
2025-06-24 15:38:28 +0800

    update homepage for volunteer

--- dris/common/views.py
+++ dris/common/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from ShelterDirectory.models import Shelter
 from DisasterReporting.models import DisasterReport
 from AidRequestManagement.models import AidRequest
-from VolunteerCoordination.models import Volunteer
+from VolunteerCoordination.models import Volunteer, VolunteerTask
 from UserManagement.models import Citizen
 
 # Create your views here.
@@ -10,6 +10,7 @@ def home_view(request):
     user = request.user
     is_citizen = False
     is_authority = False
+    is_volunteer = False
     show_disaster_and_aid = False
     show_all_cards = False
     show_shelter_and_volunteer = False
@@ -18,6 +19,7 @@ def home_view(request):
     aid_request_count = 0
     shelter_count = 0
     volunteer_count = 0
+    assigned_task_count = 0
 
     if user.is_authenticated and hasattr(user, 'role') and user.role:
         role_name = user.role.name.lower()
@@ -35,6 +37,14 @@ def home_view(request):
             aid_request_count = AidRequest.objects.count()
             shelter_count = Shelter.objects.count()
             volunteer_count = Volunteer.objects.count()
+        elif role_name == 'volunteers':
+            is_volunteer = True
+            volunteer = Volunteer.objects.filter(user=user).first()
+            show_shelter_and_volunteer = True
+            shelter_count = Shelter.objects.count()
+            volunteer_count = Volunteer.objects.count()
+            if volunteer:
+                assigned_task_count = VolunteerTask.objects.filter(volunteers=volunteer).count()
     elif not user.is_authenticated:
         show_shelter_and_volunteer = True
         shelter_count = Shelter.objects.count()
@@ -43,6 +53,7 @@ def home_view(request):
     context = {
         'is_citizen': is_citizen,
         'is_authority': is_authority,
+        'is_volunteer': is_volunteer,
         'show_disaster_and_aid': show_disaster_and_aid,
         'show_all_cards': show_all_cards,
         'show_shelter_and_volunteer': show_shelter_and_volunteer,
@@ -50,5 +61,6 @@ def home_view(request):
         'aid_request_count': aid_request_count,
         'shelter_count': shelter_count,
         'volunteer_count': volunteer_count,
+        'assigned_task_count': assigned_task_count,
     }
     return render(request, 'components/home/index.html', context)
